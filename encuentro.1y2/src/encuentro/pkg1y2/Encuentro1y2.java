7b0ba812849545fa8bd0fe8420c827fd37c34460
juan buru <juanburu8808@gmail.com>
2023-03-29 15:14:08 -0300

    mis ejerciocios 01

---
+++ encuentro.1y2/src/encuentro/pkg1y2/Encuentro1y2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package encuentro.pkg1y2;
+
+/**
+ *
+ * @author Juan
+ */
+public class Encuentro1y2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
